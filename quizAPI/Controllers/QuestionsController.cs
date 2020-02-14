dbf3248d060345642de7c98f473ce07252a338ba
rahulh25 <handoo.r@northeastern.edu>
2020-02-13 22:08:26 -0500

    Initial Commit

---
+++ quizAPI/Controllers/QuestionsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quizAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        readonly QuizContext context;
+        public QuestionsController(QuizContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+
+            return new Models.Question[]
+            {
+                new Models.Question(){Text="test"},
+                new Models.Question(){Text="test2"},
+            };
+        }
+        [HttpPost]
+        public void Post([FromBody]Models.Question question)
+        {
+            context.Questions.Add(new Models.Question() { Text="test Context"});
+        }
+    }
+}
---
+++ quizAPI/QuizContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quizAPI
+{
+    public class QuizContext: DbContext
+    {
+        public QuizContext(DbContextOptions<QuizContext> options): base(options){ }
+        
+        public DbSet<Models.Question> Questions { get; set; }
+        
+    }
+}
